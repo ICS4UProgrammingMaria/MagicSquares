bd0b258ea5ab7e88f5e3137284bc21c4f402e569
Maria <36854099+MariaT7@users.noreply.github.com>
2020-04-10 20:40:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * Created by: Maria Temu
+ * Created on: 10-April-2020
+ * Created for: ICS4U
+ * Day #29 (Magic Squares)
+ * This program generates all the magic squares
+ * with a number equal to 15
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Magic Squares");
+
+    }
+
+
+
+}
